6c5e303b467280641bbb7fbb2d2b1fb3f9525c3e
Bathily Yahaya <developpeur.yb@gmail.com>
2023-05-12 00:00:15 +0200

    ajout du controller help, mise à jour du design de la page help

---
+++ nesridiscount_gestionnaire/src/main/java/nesridiscount/controllers/HelpController.java
@@ -0,0 +1,11 @@
+package nesridiscount.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+
+public class HelpController {
+
+    @FXML
+    private ChoiceBox<?> subjectChooser;
+
+}
